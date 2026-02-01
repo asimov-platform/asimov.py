b5c288bc2bf9703097291fd6393d1c4a3c8e11c7
Arto Bendiken <arto@asimov.systems>
2026-02-01 06:34:45 -0800

    Update the project template.

--- Makefile
+++ Makefile
@@ -1,12 +1,7 @@
-PYTEST = pytest
-PYTHON = python3
-
-all: pyproject.toml
-	$(PYTHON) -m build
+all:
 
 check:
 
 clean:
-	rm -rf *~ .pytest_cache dist asimov-*.tar.gz asimov-*.whl
 
 .PHONY: all check clean
--- src/asimov/__init__.py
+++ src/asimov/__init__.py
@@ -1,11 +1,9 @@
 # This is free and unencumbered software released into the public domain.
 
-"""The Asimov Software Development Kit (SDK) for Python."""
+"""ASIMOV Software Development Kit (SDK) for Python"""
 
 import sys
 
-assert sys.version_info >= (3, 9), "The Asimov SDK for Python requires Python 3.9+"
+assert sys.version_info >= (3, 9), "ASIMOV.py requires Python 3.9+"
 
-__all__ = [
-    'sdk',
-]
+from ._version import __version__, __version_tuple__
